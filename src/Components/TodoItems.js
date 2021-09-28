a18efeb8738b8b5726adb1d140c1307f70437899
IamTarunG <tarungudipalli5@gmail.com>
2021-09-28 12:07:31 +0530

    Added style

--- src/Components/TodoItems.js
+++ src/Components/TodoItems.js
@@ -2,15 +2,15 @@ import React from "react";
 
 export default function TodoItems(props) {
   return (
-    <div className="container my-3">
+    <div className="my-3 mx-5 row">
       {props.data.map((ele) => {
         return (
           <div
-            className="card container my-3"
+            className="card my-3 mx-3 col-md-4"
             key={ele.id}
             style={{ width: "18rem" }}
           >
-            <div className="card-body">
+            <div className="card-body col-sm" id="cardBody">
               <h5 className="card-title">{ele.title}</h5>
               <p className="card-text">{ele.description}</p>
               <button
